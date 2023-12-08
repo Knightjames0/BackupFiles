62d9f0711abd3a8d7540f7e3312caa2494d49339
James <79947884+Knightjames7@users.noreply.github.com>
2023-12-08 14:51:08 -0800

    Add files via upload

--- BackupFiles/Program.cs
+++ BackupFiles/Program.cs
@@ -1,58 +1,23 @@
-﻿
+﻿using Util;
+
 namespace BackUp{
     public class Program{
-        public struct Args{
-            public string command;
-            public string[] arguments;
-        }
         public static void Main(){
             string input = "";
-            Info.CreateLog();
-            Info.CreateDataFile();
-            Info.WriteLog("New Session Started");
+            ModifyFiles.CreateLog();
+            ModifyFiles.CreateDataFile();
+            ModifyFiles.WriteLog("New Session Started");
+            Data data = new();
+
             while(input != "exit"){
                 Console.Write(">");
                 input = (Console.ReadLine() + "").Trim();
-                Args args = ParseArgs(input);
+                Args args = Utils.ParseArgs(input);
                 
                 if(args.command == "add"){
-                    Command.Add(args.arguments);
+                    data.Add(args.arguments);
                 }
             }
-        }
-        public static Args ParseArgs(string s){
-            Args args = new();
-            if(s.Length < 1){
-                Console.WriteLine("No command found");
-                return args;
-            }
-            int index = s.IndexOf(' ');
-            if(index == -1){
-                index = s.Length - 1;
-                args.command = s[0..index];
-                return args;
-            }
-            args.command = s[0..index];
-            List<string> tempList = new();
-            bool skipSpace = false;
-            string temp = "";
-            for(int i = index; i < s.Length; i++){
-                char c = s[i];
-                if(c == '"'){
-                    skipSpace = !skipSpace;
-                }else if(!skipSpace && c == ' '){
-                    if(temp != ""){
-                        tempList.Add(temp);
-                        temp = "";
-                    }
-                    continue;
-                }else{
-                    temp += c;
-                }
-            }
-            tempList.Add(temp);
-            args.arguments = tempList.ToArray();
-            return args;
         }
     }
 }
--- BackupFiles/Scripts/AddFiles.cs
+++ BackupFiles/Scripts/AddFiles.cs
@@ -1,17 +1,42 @@
-using static Utils;
+using Util;
 
 namespace BackUp{
-    public class Command{
-        public static List<Data_Paths> listData = new();
-
-        public static void Add(string[] arguments){ //Check if file or directory exists
+    public class Data{
+        private List<Data_Path> listData;
+        public Data(){
+            listData = ModifyFiles.ReadData();
+            foreach(Data_Path data_ in listData){
+                Console.WriteLine(data_.ToString());
+            }
+        }
+        public bool HasPath(Data_Path data_Path){ // TODO
+            bool result = false;
+            Parallel.ForEach(listData, data => {
+                if(data.path == data_Path.path){
+                    result = true;
+                }
+            });
+            return result;
+        }
+        
+        public void Add(string[] arguments){ //Check if file or directory exists
             foreach(string arg in arguments){
+                char type;
                 if(File.Exists(arg)){
-                    Info.WriteData(arg,'-');
+                    type = '-';
                 }else if(Directory.Exists(arg)){
-                    Info.WriteData(arg,'d');
+                    type = 'd';
                 }else{
-                    Console.WriteLine("Error occured file or directory dosen't exist: " + arg);
+                    Console.WriteLine("Error occured file or directory path dosen't exist or can't be reached: " + arg);
+                    ModifyFiles.WriteLog("Error occured file or directory path dosen't exist or can't be reached: " + arg);
+                    continue;
+                }
+                Data_Path data = new(type,arg);
+                if(!HasPath(data)){ //check if already exists in listData
+                    ModifyFiles.WriteData(data);
+                    listData.Add(data);
+                }else{
+                    Console.WriteLine("Error already in list of files: " + arg);
                 }
             }
         }
